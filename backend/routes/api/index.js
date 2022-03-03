d3d7c7b4194f08fc55bf47209bc1a2bddddfb4cf
Nick Austin <nickaustin@Nicks-Mac-mini.local>
2022-03-03 15:17:14 -0700

    login and signup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,8 +6,8 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
